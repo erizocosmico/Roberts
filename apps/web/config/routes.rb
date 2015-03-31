b5626f13ecd17872d331979ec491a9f9f0c4b38a
Miguel Molina <hi@mvader.me>
2015-03-31 20:12:20 +0200

    Routing

--- apps/web/config/routes.rb
+++ apps/web/config/routes.rb
@@ -1,8 +1,13 @@
-# Configure your routes here
-# See: http://www.rubydoc.info/gems/lotus-router/#Usage
-#
-#
-#
-# This route will look for `Web::Controllers::Home::Index` action in `apps/web/controllers/home/index.rb`.
-# Please, uncomment the following line to have a working example.
-# get '/', to: 'home#index'
+get '/' to: 'tickets#list'
+
+resources 'tickets', except: [:index] do
+  collection do
+    get 'list/page/:page', to: 'tickets#list'
+  end
+end
+
+namespace 'user' do
+  get '/login', to: 'user#login'
+  post '/login', to: 'user#enter'
+  get '/logout', to: 'user#logout'
+end
